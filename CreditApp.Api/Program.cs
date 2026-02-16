59cb056c0899850e659a7d59ad2b059a94a09fed
Ivan K <buckingkiska@gmail.com>
2026-02-16 17:52:00 +0400

    фикс проблемы с CORS

--- CreditApp.Api/Program.cs
+++ CreditApp.Api/Program.cs
@@ -2,21 +2,36 @@ using CreditApp.Api.Services.CreditGeneratorService;
 using CreditApp.ServiceDefaults;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.AddServiceDefaults();
+
 builder.AddRedisClient("cache");
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     var connectionString = builder.Configuration.GetConnectionString("cache");
     options.Configuration = connectionString;
 });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazorWasm", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddScoped<ICreditApplicationGeneratorService, CreditApplicationGeneratorService>();
+
+// Add controllers
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -24,6 +39,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowBlazorWasm");
 app.MapControllers();
 app.MapDefaultEndpoints();
 
