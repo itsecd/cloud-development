fa78a55e3ad5d0d7b565bf7294f9e78abc313d0a
Matvey Bogachev <boga4iov.matvei2016@yandex.ru>
2026-02-18 01:25:49 +0400

    создан -класс транспортное средство - изменена информация о студенте на странице

---
+++ Domain/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain;
+
+public class Vehicle
+{
+    public int Id { get; set; }
+    public string VinNumber { get; set; }
+    public string Maker { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string TypeBodyCar { get; set; }
+    public string TypeFuel { get; set; }
+    public string Сolor { get; set; }
+    public double Mileage { get; set; }
+    public DateOnly LastMaintenance {get; set; }
+}
