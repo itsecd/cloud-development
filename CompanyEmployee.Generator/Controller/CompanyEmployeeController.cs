95f76c8a6fe29343c095fb51848acd0b3163bbf8
dedrowe <barushnikovvs@gmail.com>
2026-02-22 17:24:07 +0400

    complete lab1

---
+++ CompanyEmployee.Generator/Controller/CompanyEmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using CompanyEmployee.Generator.Dto;
+using CompanyEmployee.Generator.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployee.Generator.Controller;
+
+[ApiController]
+[Route("company-employee")]
+public class CompanyEmployeeController(
+    CompanyEmployeeService service
+    ) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<CompanyEmployeeDto>> GetById([FromQuery] int id, CancellationToken cancellationToken)
+    {
+        var employee = await service.GetByIdAsync(id, cancellationToken);
+        return Ok(employee);
+    }
+}
