1c6352f24a3c1611416b73e8933bf3b88a6c160f
Donistr <deniselag@mail.ru>
2026-03-05 17:33:24 +0400

    refactor: убрал бесполезный cors у сервиса-генератора

--- ResidentialBuilding.Generator/Program.cs
+++ ResidentialBuilding.Generator/Program.cs
@@ -7,25 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 builder.AddRedisDistributedCache("residential-building-cache");
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowLocalDev", policy =>
-    {
-        policy
-            .AllowAnyOrigin()
-            .WithHeaders("Content-Type")
-            .WithMethods("GET");
-    });
-});
-
 builder.Services.AddSingleton<ResidentialBuildingGenerator>();
 builder.Services.AddSingleton<IResidentialBuildingService,  ResidentialBuildingService>();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
-app.UseCors("AllowLocalDev");
 
 app.MapControllers();
 
