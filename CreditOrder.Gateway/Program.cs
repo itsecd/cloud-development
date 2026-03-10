c0f3400d1b6a5d2dca951cb830f324032bf46c4e
acebiscuits <27roter72@gmail.com>
2026-03-10 19:20:17 +0400

    add: ocelot api gateway

---
+++ CreditOrder.Gateway/Program.cs
@@ -0,0 +1,25 @@
+using CreditOrder.Gateway.LoadBalancers;
+using Ocelot.DependencyInjection;
+using Ocelot.LoadBalancer.Interfaces;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services
+    .AddOcelot(builder.Configuration)
+    .AddCustomLoadBalancer<WeightedRandomLoadBalancer>((serviceProvider, _, discoveryProvider) =>
+    {
+        var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+        return new WeightedRandomLoadBalancer(
+            configuration,
+            discoveryProvider!.GetAsync);
+    });
+
+builder.Configuration.AddOcelot();
+
+var app = builder.Build();
+await app.UseOcelot();
+await app.RunAsync();
