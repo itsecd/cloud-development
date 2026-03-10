2dbb56dbf89ceb98075d08e086057a8063c265ad
acebiscuits <27roter72@gmail.com>
2026-03-10 19:21:54 +0400

    add: replicas; weighted-random load balancer

--- CloudDevelopment.AppHost/AppHost.cs
+++ CloudDevelopment.AppHost/AppHost.cs
@@ -1,16 +1,38 @@
+using Microsoft.Extensions.Configuration;
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var ports = builder.Configuration.GetSection("ApiService:Ports").Get<int[]>()
+           ?? throw new InvalidOperationException("ApiService:Ports is not  configured.");
+
 var cache = builder.AddRedis("credit-order-cache")
     .WithRedisInsight(containerName: "credit-order-insight");
 
-var generator = builder.AddProject<Projects.Generator>("generator")
-    .WithReference(cache, "RedisCache")
-    .WaitFor(cache);
+var generators = new List<IResourceBuilder<ProjectResource>>();
+
+for (var i = 0; i < ports.Length; i++)
+{
+    var httpsPort = ports[i];
+    var httpPort = ports[i] - 1000;
+    var urls = $"https://localhost:{httpsPort};http://localhost:{httpPort}";
+
+    generators.Add(
+        builder.AddProject<Projects.Generator>($"generator-r{i + 1}")
+            .WithReference(cache, "RedisCache")
+            .WithEnvironment("ASPNETCORE_URLS", urls)
+            .WaitFor(cache)
+    );
+}
+
+var gateway = builder.AddProject<Projects.CreditOrder_Gateway>("gateway");
+foreach (var g in generators)
+{
+    gateway.WaitFor(g);
+}
 
 var client = builder.AddProject<Projects.Client_Wasm>("credit-order-wasm")
-    .WithReference(generator)
-    .WaitFor(generator);
+    .WithReference(gateway)
+    .WaitFor(gateway);
 
 builder.Build().Run();
