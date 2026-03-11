e0020ea6703a218d991e5ec770af989e4bea4230
the80hz <dnl.vlgv@mail.ru>
2026-03-11 17:03:19 +0400

    Лаб 1: обновлен AppHost до Aspire 9.5 с WaitFor и RedisInsight

--- CloudDevelopment.AppHost/Program.cs
+++ CloudDevelopment.AppHost/Program.cs
@@ -1,11 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var redis = builder.AddRedis("redis");
+var redis = builder.AddRedis("redis")
+    .WithRedisInsight(containerName: "redis-insight");
 
 builder.AddProject("course-generator-api", "../CourseGenerator.Api/CourseGenerator.Api.csproj")
     .WithHttpEndpoint(name: "http")
     .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
     .WithReference(redis)
+    .WaitFor(redis)
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
