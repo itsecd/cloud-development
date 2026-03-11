9e1a3962cacfb217e0a266f71e8763781e782fbb
the80hz <dnl.vlgv@mail.ru>
2026-03-11 17:04:02 +0400

    Лаб 1: добавлен Client.Wasm в оркестрацию AppHost

--- CloudDevelopment.AppHost/Program.cs
+++ CloudDevelopment.AppHost/Program.cs
@@ -3,11 +3,16 @@ var builder = DistributedApplication.CreateBuilder(args);
 var redis = builder.AddRedis("redis")
     .WithRedisInsight(containerName: "redis-insight");
 
-builder.AddProject("course-generator-api", "../CourseGenerator.Api/CourseGenerator.Api.csproj")
+var courseGeneratorApi = builder.AddProject("course-generator-api", "../CourseGenerator.Api/CourseGenerator.Api.csproj")
     .WithHttpEndpoint(name: "http")
     .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
     .WithReference(redis)
     .WaitFor(redis)
+    .WithExternalHttpEndpoints();
+
+builder.AddProject("client-wasm", "../Client.Wasm/Client.Wasm.csproj")
+    .WithReference(courseGeneratorApi)
+    .WaitFor(courseGeneratorApi)
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
