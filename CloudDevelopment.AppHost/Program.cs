d5265028e743c59620b87cce9dcd3ec267f60933
the80hz <dnl.vlgv@mail.ru>
2026-03-11 16:46:38 +0400

    Лаб 1: добавлена настройка окружения для проекта course-generator-api

--- CloudDevelopment.AppHost/Program.cs
+++ CloudDevelopment.AppHost/Program.cs
@@ -4,6 +4,7 @@ var redis = builder.AddRedis("redis");
 
 builder.AddProject("course-generator-api", "../CourseGenerator.Api/CourseGenerator.Api.csproj")
     .WithHttpEndpoint(name: "http")
+    .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
     .WithReference(redis)
     .WithExternalHttpEndpoints();
 
