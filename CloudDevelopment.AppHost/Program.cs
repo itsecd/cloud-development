34c232fb0c85f9bd7f34c63ca9940b396e352535
the80hz <dnl.vlgv@mail.ru>
2026-03-11 15:45:51 +0400

    Лаб 1: добавлена поддержка внешних HTTP-эндпоинтов для генератора курсов

--- CloudDevelopment.AppHost/Program.cs
+++ CloudDevelopment.AppHost/Program.cs
@@ -3,6 +3,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 var redis = builder.AddRedis("redis");
 
 builder.AddProject("course-generator-api", "../CourseGenerator.Api/CourseGenerator.Api.csproj")
-    .WithReference(redis);
+    .WithHttpEndpoint(name: "http")
+    .WithReference(redis)
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
