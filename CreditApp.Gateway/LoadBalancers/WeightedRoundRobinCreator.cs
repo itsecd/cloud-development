e8abbf920c028cffd28eb664d44b01d0a3b5dbc4
Rifinn-crypto <114596909+Rifinn-crypto@users.noreply.github.com>
2026-03-12 21:46:56 +0400

    Настройка клиента
    8 часов превосходного времяприпровождения в поисках ошибки, которая оказалась в протоколах)

---
+++ CreditApp.Gateway/LoadBalancers/WeightedRoundRobinCreator.cs
@@ -0,0 +1,26 @@
+﻿using Ocelot.Configuration;
+using Ocelot.LoadBalancer;
+using Ocelot.LoadBalancer.Interfaces;
+using Ocelot.Responses;
+using Ocelot.ServiceDiscovery.Providers;
+using Ocelot.Values;
+
+namespace CreditApp.Gateway.LoadBalancers;
+
+public class WeightedRoundRobinCreator : ILoadBalancerCreator
+{
+    public string Type => "WeightedRoundRobin";
+
+    public Response<ILoadBalancer> Create(
+        DownstreamRoute route,
+        IServiceDiscoveryProvider serviceDiscoveryProvider)
+    {
+        var services = route.DownstreamAddresses
+            .Select(x => new ServiceHostAndPort(x.Host, x.Port))
+            .ToList();
+
+        var balancer = new WeightedRoundRobinLoadBalancer(services);
+
+        return new OkResponse<ILoadBalancer>(balancer);
+    }
+}
---
+++ CreditApp.Gateway/LoadBalancers/WeightedRoundRobinLoadBalancer.cs
@@ -0,0 +1,44 @@
+﻿using Ocelot.LoadBalancer;
+using Ocelot.LoadBalancer.Interfaces;
+using Ocelot.Responses;
+using Ocelot.Values;
+
+namespace CreditApp.Gateway.LoadBalancers;
+
+public class WeightedRoundRobinLoadBalancer : ILoadBalancer
+{
+    private readonly List<ServiceHostAndPort> _sequence;
+    private int _index = -1;
+    private readonly object _lock = new();
+
+    public string Type => "WeightedRoundRobin";
+
+    public WeightedRoundRobinLoadBalancer(List<ServiceHostAndPort> services)
+    {
+        var weights = new[] { 3, 2, 1, 1, 1 };
+        _sequence = [];
+
+        for (var i = 0; i < services.Count; i++)
+        {
+            var weight = weights[i];
+            for (var j = 0; j < weight; j++)
+            {
+                _sequence.Add(services[i]);
+            }
+        }
+    }
+
+    public Task<Response<ServiceHostAndPort>> LeaseAsync(HttpContext context)
+    {
+        lock (_lock)
+        {
+            _index = (_index + 1) % _sequence.Count;
+            return Task.FromResult<Response<ServiceHostAndPort>>(
+                new OkResponse<ServiceHostAndPort>(_sequence[_index]));
+        }
+    }
+
+    public void Release(ServiceHostAndPort hostAndPort)
+    {
+    }
+}
