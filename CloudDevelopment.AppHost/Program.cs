80b42cc64ee6a6a8a49ee8ed40ac5c8d7d120ae7
the80hz <dnl.vlgv@mail.ru>
2026-03-12 14:31:15 +0400

    Лаб 1: добавлены ссылки на проекты Client.Wasm и CourseGenerator.Api в AppHost

--- CloudDevelopment.AppHost/Program.cs
+++ CloudDevelopment.AppHost/Program.cs
@@ -3,14 +3,14 @@ var builder = DistributedApplication.CreateBuilder(args);
 var redis = builder.AddRedis("redis")
     .WithRedisInsight(containerName: "redis-insight");
 
-var courseGeneratorApi = builder.AddProject("course-generator-api", "../CourseGenerator.Api/CourseGenerator.Api.csproj")
+var courseGeneratorApi = builder.AddProject<Projects.CourseGenerator_Api>("course-generator-api")
     .WithHttpEndpoint(name: "http")
     .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
     .WithReference(redis)
     .WaitFor(redis)
     .WithExternalHttpEndpoints();
 
-builder.AddProject("client-wasm", "../Client.Wasm/Client.Wasm.csproj")
+builder.AddProject<Projects.Client_Wasm>("client-wasm")
     .WithReference(courseGeneratorApi)
     .WaitFor(courseGeneratorApi)
     .WithExternalHttpEndpoints();
