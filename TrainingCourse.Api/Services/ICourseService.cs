f54c1925277ab4c8cafdb3dc6a68700306230157
Colnuwko <Vladislav290403@gmail.com>
2026-03-15 13:04:53 +0400

    Добавил эндпоинт, скорректировал настройки для CORS, Подключил кэширование

---
+++ TrainingCourse.Api/Services/ICourseService.cs
@@ -0,0 +1,16 @@
+﻿using TrainingCourse.Api.Models;
+
+namespace TrainingCourse.Api.Services;
+
+/// <summary>
+/// Интерфейс сервиса для работы с курсами
+/// </summary>
+public interface ICourseService
+{
+    /// <summary>
+    /// Получить курс по идентификатору
+    /// </summary>
+    /// <param name="id">Идентификатор курса</param>
+    /// <returns>Модель курса</returns>
+    public Task<Course> GetCourse(int id);
+}
