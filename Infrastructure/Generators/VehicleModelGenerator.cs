975f080c66b4319b5022e62132b2322a1a528b57
Matvey Bogachev <boga4iov.matvei2016@yandex.ru>
2026-03-18 21:01:15 +0400

    Удалены ненужные закомиченые исключения и один файл = один класс

--- Infrastructure/Generators/VehicleModelGenerator.cs
+++ Infrastructure/Generators/VehicleModelGenerator.cs
@@ -7,14 +7,6 @@ using System.Text.Json;
 
 namespace Infrastructure.Generators;
 
-/// <summary>
-/// Класс для хранения производителя и всех его моделей
-/// </summary>
-public class VehicleModelJsonItem
-{
-    public required string Make { get; set; }
-    public List<string> Models { get; set; } = new();
-}
 
 /// <summary>
 ///  Класс для генерации правильных данных производитель + модель 
@@ -65,7 +57,6 @@ public class VehicleModelGenerator : IVehicleModelGenerator
 
         if (!File.Exists(_filePath))
             _logger.LogWarning("Dataset file not found. FilePath: {FilePath}", _filePath);
-        //throw new FileNotFoundException($"Файл не найден: {_filePath}");
 
 
         var json = File.ReadAllText(_filePath);
@@ -79,7 +70,6 @@ public class VehicleModelGenerator : IVehicleModelGenerator
 
         if (data is null || data.Count == 0)
             _logger.LogWarning("File is empty or corrupted. FilePath: {FilePath}", _filePath);
-        //throw new InvalidOperationException("Файл vehicle models.json пустой или поврежден.");
 
         data = data
             .Where(x => !string.IsNullOrWhiteSpace(x.Make) && x.Models is not null && x.Models.Count > 0)
@@ -87,7 +77,6 @@ public class VehicleModelGenerator : IVehicleModelGenerator
 
         if (data.Count == 0)
             _logger.LogWarning("No valid makes and models in file. FilePath: {FilePath}", _filePath);
-        //throw new InvalidOperationException("В файле нет валидных производителей и моделей.");
 
         _items = data;
         _logger.LogInformation("Make and model data from file loaded successfully. FilePath: {FilePath}", _filePath);
---
+++ Infrastructure/Generators/VehicleModelJsonItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Infrastructure.Generators;
+
+/// <summary>
+/// Класс для хранения производителя и всех его моделей
+/// </summary>
+public class VehicleModelJsonItem
+{
+    /// <summary>
+    /// Поле производитель ТО
+    /// </summary>
+    public required string Make { get; set; }
+    /// <summary>
+    /// Поле Модель ТО
+    /// </summary>
+    public List<string> Models { get; set; } = new();
+}
