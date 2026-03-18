50be3f3a2cd780b814ef89970670f4a959a55209
Matvey Bogachev <boga4iov.matvei2016@yandex.ru>
2026-03-18 22:26:07 +0400

    порт вынесен в appsetting.json

--- Client.Wasm/Program.cs
+++ Client.Wasm/Program.cs
@@ -2,7 +2,6 @@ using Blazorise;
 using Blazorise.Bootstrap;
 using Blazorise.Icons.FontAwesome;
 using Client.Wasm;
-using Domain;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -10,17 +9,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp =>
-    new HttpClient
-    {
-        BaseAddress = new Uri("https://localhost:7087/")
-    });
+var baseAddress = builder.Configuration["BaseAddress"]
+    ?? throw new InvalidOperationException("BaseAddress is not configured.");
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
 
 builder.Services.AddBlazorise(options => { options.Immediate = true; })
     .AddBootstrapProviders()
     .AddFontAwesomeIcons();
-
 
 await builder.Build().RunAsync();
