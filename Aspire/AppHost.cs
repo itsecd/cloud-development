46b43a6423375f027742ffb5e9fddea048951569
Matvey Bogachev <boga4iov.matvei2016@yandex.ru>
2026-03-19 15:18:35 +0400

    Удален ненужный проект

--- Aspire/AppHost.cs
+++
@@ -1,9 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var cache = builder.AddRedis("cache");
-
-builder.AddProject<Asp>("api")
-       .WithReference(cache);
-
-
-builder.Build().Run();
