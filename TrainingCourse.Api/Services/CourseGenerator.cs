ebb2276140203a8104b5b088c2c679c462feba25
Colnuwko <Vladislav290403@gmail.com>
2026-03-19 23:26:51 +0400

    Сделал правки.

--- TrainingCourse.Api/Services/CourseGenerator.cs
+++ TrainingCourse.Api/Services/CourseGenerator.cs
@@ -9,14 +9,10 @@ using TrainingCourse.Api.Models;
 public static class CourseGenerator
 {
     private static readonly Faker<Course> _faker = new Faker<Course>()
-        .RuleFor(c => c.CourseName, f => f.Company.CatchPhrase() + " курс")
+        .RuleFor(c => c.CourseName, f => f.Company.CatchPhrase() + " course")
         .RuleFor(c => c.TeacherFullName, f => f.Name.FullName())
-        .RuleFor(c => c.StartDate, f => DateOnly.FromDateTime(f.Date.Future(1)))
-        .RuleFor(c => c.EndDate, (f, c) =>
-        {
-            var startDateTime = c.StartDate.ToDateTime(TimeOnly.MinValue);
-            return DateOnly.FromDateTime(f.Date.Between(startDateTime, startDateTime.AddMonths(6)));
-        })
+        .RuleFor(c => c.StartDate, f => f.Date.FutureDateOnly(1))
+        .RuleFor(c => c.EndDate, (f, c) => f.Date.BetweenDateOnly(c.StartDate, c.StartDate.AddMonths(6)))
         .RuleFor(c => c.MaxStudents, f => f.Random.Int(10, 50))
         .RuleFor(c => c.CurrentStudents, (f, c) => f.Random.Int(0, c.MaxStudents))
         .RuleFor(c => c.HasCertificate, f => f.Random.Bool(0.8f)) // 80% вероятность выдачи сертификата
