adf4c8b8607e011067c6134b66769917beab7cf5
egors <egors@DESKTOP-LA93FBK>
2026-04-14 10:26:27 +0300

    подправил таблицу данных

--- GenerationService/Models/SoftwareProjectContract.cs
+++ GenerationService/Models/SoftwareProjectContract.cs
@@ -1,14 +1,14 @@
 ﻿namespace GenerationService.Models;
 
-
 public record SoftwareProjectContract(
-    Guid Id,               
-    string ProjectName,    
-    string ClientCompany,  
-    string TechStack,      
-    int TeamSize,          
-    decimal Budget,       
-    DateTime StartDate,  
-    DateTime Deadline,   
-    string ProjectManager 
+    int Id,                      
+    string ProjectName,        
+    string ClientCompany,      
+    string ProjectManager,     
+    DateOnly StartDate,         
+    DateOnly PlannedEndDate,   
+    DateOnly ActualEndDate,      
+    decimal Budget,             
+    decimal ActualCost,           
+    int CompletionPercentage      
 );
