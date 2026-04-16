c02c0227f9851542f2f7872b7f21650df830a2a4
Mishachuu <mishakrut11@gmail.com>
2026-04-16 11:28:09 +0400

    fix

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -8,6 +8,29 @@ builder.AddServiceDefaults();
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
+var ocelotConfig = builder.Configuration.GetSection("Routes:0");
+var hosts = new List<object>();
+
+for (var i = 1; ; i++)
+{
+    var url = builder.Configuration[$"services__vehicleapi-{i}__http__0"];
+    if (url == null) break;
+
+    var uri = new Uri(url);
+    hosts.Add(new { Host = uri.Host, Port = uri.Port });
+}
+
+if (hosts.Count > 0)
+{
+    builder.Configuration["Routes:0:DownstreamHostAndPorts"] = null;
+    for (var i = 0; i < hosts.Count; i++)
+    {
+        var h = (dynamic)hosts[i];
+        builder.Configuration[$"Routes:0:DownstreamHostAndPorts:{i}:Host"] = h.Host;
+        builder.Configuration[$"Routes:0:DownstreamHostAndPorts:{i}:Port"] = h.Port.ToString();
+    }
+}
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
