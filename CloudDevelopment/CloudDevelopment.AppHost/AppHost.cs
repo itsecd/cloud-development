2b3bdec0448b21a13b30958bd166f2275eb773aa
egors <egors@DESKTOP-LA93FBK>
2026-04-29 12:18:44 +0300

    попрвил. Удалил файлы и папки(не были видны в vs. Wasm запускается, скрины в отдельно папке(может мешать vpn). redis есть

--- CloudDevelopment/CloudDevelopment.AppHost/AppHost.cs
+++ CloudDevelopment/CloudDevelopment.AppHost/AppHost.cs
@@ -1,9 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var redis = builder.AddRedis("redis");
+var redis = builder.AddRedis("redis")
+    .WithRedisInsight(); // добавляет веб-интерфейс Redis Insight
 
 builder.AddProject<Projects.GenerationService>("generation-service")
     .WithReference(redis)      
-    .WaitFor(redis);            
+    .WaitFor(redis);
+
+builder.AddProject<Projects.Client_Wasm>("client-wasm");
 
 builder.Build().Run();
