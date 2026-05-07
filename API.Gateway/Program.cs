58594c917934c271e3efdb2472e39262fba6fbd6
neygenius <mr.neygen@yandex.ru>
2026-05-07 08:53:00 +0400

    вторая лаба

---
+++ API.Gateway/Program.cs
@@ -0,0 +1,34 @@
+using API.Gateway.LoadBalancers;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("LocalPolicy", policy =>
+    {
+        policy
+            .SetIsOriginAllowed(origin => origin.StartsWith("https://localhost"))
+            .WithHeaders("Content-Type")
+            .WithMethods("GET");
+    });
+});
+
+builder.Configuration
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services
+    .AddOcelot(builder.Configuration)
+    .AddCustomLoadBalancer((sp, _, discoveryProvider) =>
+    {
+        var configuration = sp.GetRequiredService<IConfiguration>();
+        return new WeightedRandomLoadBalancer(discoveryProvider.GetAsync, configuration);
+    });
+
+var app = builder.Build();
+
+app.UseCors("LocalPolicy");
+await app.UseOcelot();
+
+await app.RunAsync();
--- Cloud.API/Program.cs
+++ Cloud.API/Program.cs
@@ -13,17 +13,6 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IEmployeeGenerator, EmployeeGenerator>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("LocalPolicy", policy =>
-    {
-        policy
-            .SetIsOriginAllowed(origin => origin.StartsWith("https://localhost"))
-            .WithHeaders("Content-Type")
-            .WithMethods("GET");
-    });
-});
-
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
@@ -35,7 +24,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseCors("LocalPolicy");
 app.MapControllers();
 
 app.Run();
