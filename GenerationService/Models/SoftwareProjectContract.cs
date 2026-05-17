ae2f81714554cba0f5bb9ca261023dce4f3471a1
egors <egors@DESKTOP-LA93FBK>
2026-05-18 00:31:11 +0300

    Исправил все по списку.

--- GenerationService/Models/SoftwareProjectContract.cs
+++ GenerationService/Models/SoftwareProjectContract.cs
@@ -3,22 +3,13 @@
 public class SoftwareProjectContract
 {
     public int Id { get; init; }
-
     public string ProjectName { get; set; } = string.Empty;
-
     public string ClientCompany { get; set; } = string.Empty;
-
     public string ProjectManager { get; set; } = string.Empty;
-
     public DateOnly StartDate { get; set; }
-
     public DateOnly PlannedEndDate { get; set; }
-
     public DateOnly? ActualEndDate { get; set; }
-
     public decimal Budget { get; set; }
-
     public decimal ActualCost { get; set; }
-
     public int CompletionPercentage { get; set; }
 }
